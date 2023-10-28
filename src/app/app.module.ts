21fdf43badd9ae74195ee4207dd05e2a7bec4a8d
Guilh <wanderfull.mystc@gmail.com>
2023-10-28 00:40:10 -0300

    Projeto criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SkuComponent } from './components/sku/sku.component';
+import { StockComponent } from './components/stock/stock.component';
+import { AccountComponent } from './components/account/account.component';
+import { AnnouncementComponent } from './components/announcement/announcement.component';
+import { LoginComponent } from './components/login/login.component';
+import { SingUpComponent } from './components/sing-up/sing-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SkuComponent,
+    StockComponent,
+    AccountComponent,
+    AnnouncementComponent,
+    LoginComponent,
+    SingUpComponent
   ],
   imports: [
     BrowserModule,
