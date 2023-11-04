0fbf84865a8166f50c4455b3b8ac69e593c9fcd6
Arthur Henrique Vieira Busanello <tuti.busanello@hotmail.com>
2023-11-04 20:50:29 -0300

    front do estoque basico

--- src/app/components/stock/stock.component.ts
+++ src/app/components/stock/stock.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Stock } from './stock';
+import { StockService } from './stock.service';
 
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
   styleUrls: ['./stock.component.scss']
 })
-export class StockComponent {
+export class StockComponent implements OnInit {
+  stock: Stock[] = [];
 
+  carroSelecionadoParaEdicao: any;
+  constructor(private stockService: StockService) { }
+  showAddStock: boolean = false;
+
+  ngOnInit(): void {
+      this.loadAllStock();
+  }
+
+  loadAllStock(): void {
+      
+      this.stockService.getAllStock().subscribe(data => {
+          this.stock = data as Stock[];
+      }, error => {
+          console.error('Error fetching all accounts:', error);
+      });
+      console.log(this.stock);
+      console.log("I will be back");
+  }
+
+  addOuEditarCarro(event: any) {
+      console.log("teste");
+  }
+
+  editStock(stockId: number) {
+      console.log(`Edit Stock with ID: ${stockId}`);
+  }
+
+  deleteAccount(stockId: number) {
+      console.log(`Delete Stock with ID: ${stockId}`);
+  }
 }
+function ngOnInit() {
+  throw new Error('Function not implemented.');
+}
+
