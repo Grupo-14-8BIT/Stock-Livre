a173faee8e1b05008adb18f8f56e5a07f764b38e
Guilh <wanderfull.mystc@gmail.com>
2023-11-05 03:22:58 -0300

    silver fix issues

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
 import { Login } from './login';
 import { Router } from '@angular/router';
 import { LoginService } from './login.service';
+import eventService from 'src/app/event.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -29,7 +31,20 @@ export class LoginComponent {
     }
 
     // Call the loginService.fetch() method to authenticate the user
-    (await this.loginService.fetch(this.login.email, this.login.senha)).subscribe( data => { console.log(data)})
+    (await this.loginService.fetch(this.login.email, this.login.senha)).subscribe( data => { 
+
+      if ( data == Error) {
+
+        alert("Usuario nao existe")
+      } else {
+
+
+        eventService.emit("usuario Logou", JSON.stringify(data) )
+
+      }
+      
+let teste = JSON.stringify(data);
+      console.log(teste)})
 
   //   // Make a request to the sing-up back end to validate the login
   //   const isLoginValid = await this.loginService.validateLogin(loginResponse.email as string, loginResponse.senha as string);
