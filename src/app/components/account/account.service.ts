5b22d79db4c3f376bd569868383ae13137368d17
Guilh <wanderfull.mystc@gmail.com>
2023-11-06 04:20:16 -0300

    Working on Cors Problems

--- src/app/components/account/account.service.ts
+++ src/app/components/account/account.service.ts
@@ -12,46 +12,32 @@ import { CookieService } from 'ngx-cookie-service';
 export class AccountService {
     private baseURL = "http://localhost:8082/api/v1/admin/conta";
 
-
-
-    constructor(private httpClient: HttpClient, private cookieService: CookieService) {
-
-
-    }
+    constructor(private httpClient: HttpClient, private cookieService: CookieService) { }
 
     token: string = this.cookieService.get("JWT");
 
-
     private getStandardOptions(): any {
-        return {
-            headers: new HttpHeaders({
-                'Content-Type': 'application/json',
-            })
-        };
-    }
+        const token = this.cookieService.get("JWT");
+        console.log(token);
+        
+        if (!token) {
+          throw new Error('JWT token not found in cookies');
+        }
     
-
-      
+        const headers = new HttpHeaders({
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        });
     
+        return { headers };
+      }
 
-
-getAllAccounts() :any {
-
-    let options = this.getStandardOptions();
-
-    console.log("getAllAccount:\t" + this.token);
-
-    options.headers = options.headers.set('Authorization', this.token)
-
-    // const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);      
-    return this.httpClient.get(`${this.baseURL}/getAll`, options);
-
-    //    const contasObservable = contas
-    //    .pipe(
-    //      map((buffer: ArrayBuffer) => JSON.parse(JSON.stringify(buffer)))
-    //    );
-
-    //    return contasObservable;
+getAllAccounts(): Observable<any> {
+    const options = this.getStandardOptions();
+    return this.httpClient.get<Account[]>(`${this.baseURL}/getAll`, options)
+        .pipe(
+        catchError(this.handleError)
+        );
 }
 
 getAccountById(id: number) {
@@ -69,14 +55,14 @@ updateAccount(id: number, accountData: any) {
 deleteAccount(id: number) {
     return this.httpClient.delete(`${this.baseURL}/conta/${id}`);
 }
+
+handleError(error: HttpErrorResponse) {
+    // Handle the HTTP error here
+    console.error('An error occurred:', error.error);
+    return throwError(() => new Error('Something bad happened; please try again later.'));
 }
-
-
+}
 
 function CrossOrigin(): (target: AccountService, propertyKey: "getAllAccounts", descriptor: TypedPropertyDescriptor<() => any>) => void | TypedPropertyDescriptor<() => any> {
     throw new Error('Function not implemented.');
 }
-// function CrossOrigin(): (target: AccountService, propertyKey: "getAllAccounts", descriptor: TypedPropertyDescriptor<() => any>) => void | TypedPropertyDescriptor<() => any> {
-//     throw new Error('Function not implemented.');
-// }
-
