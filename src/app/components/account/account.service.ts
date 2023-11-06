5e25774aea5e3f130b49d1dea76d1e7b24b06d45
Guilh <wanderfull.mystc@gmail.com>
2023-11-06 18:55:11 -0300

    done 7/10 account

--- src/app/components/account/account.service.ts
+++ src/app/components/account/account.service.ts
@@ -11,16 +11,9 @@ import { CookieService } from 'ngx-cookie-service';
 })
 export class AccountService {
     private baseURL = "http://localhost:8082/api/v1/admin/conta";
-
-
-
-    constructor(private httpClient: HttpClient, private cookieService: CookieService) {
-
-
-    }
-
     token: string = this.cookieService.get("JWT");
 
+    constructor(private httpClient: HttpClient, private cookieService: CookieService) {}
 
     private getStandardOptions(): any {
         return {
@@ -29,23 +22,14 @@ export class AccountService {
             })
         };
     }
-    
-
-      
-    
-
 
 getAllAccounts() :any {
 
     let options = this.getStandardOptions();
 
-    console.log("getAllAccount:\t" + this.token);
-
-    
+    console.log(this.token);
 
     options.headers = options.headers.set('Authorization', `Bearer ${this.token}`)
-
-
 
     // const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);      
     return this.httpClient.get(`${this.baseURL}/getAll`, options);
@@ -62,6 +46,18 @@ getAccountById(id: number) {
     return this.httpClient.get(`${this.baseURL}/conta/${id}`);
 }
 
+autorizaAccount(): Observable<any> {
+    let options = this.getStandardOptions();
+    options.headers = options.headers.set('Authorization', `Bearer ${this.token}`);
+    // Note: 'as any'    cast is required to avoid TypeScript compile errors
+    options.responseType = 'text' as 'json'; // Set responseType to 'text' as 'json'
+  
+    // Use a GET request and pass the modified options, cast options as 'any'
+    return this.httpClient.get<string>(`${this.baseURL}/autoriza`, options as any);
+  }
+
+  
+
 createAccount(accountData: any) {
     return this.httpClient.post(`${this.baseURL}/conta`, accountData);
 }
@@ -74,9 +70,6 @@ deleteAccount(id: number) {
     return this.httpClient.delete(`${this.baseURL}/conta/${id}`);
 }
 }
-
-
-
 // function CrossOrigin(): (target: AccountService, propertyKey: "getAllAccounts", descriptor: TypedPropertyDescriptor<() => any>) => void | TypedPropertyDescriptor<() => any> {
 //     throw new Error('Function not implemented.');
 // }
